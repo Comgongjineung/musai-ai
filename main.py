ebd161711a4b5db49bcb4844c3f7004d8e38b471
RYU <rjw5256@gmail.com>
2025-06-22 17:04:03 +0900

    Revert "✨ Feat: ADD gpt api로 변경"
    This reverts commit 40c4e35594a9e3dd893c8f8436f66edc433c953d.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 from routes.vision_routes import router as vision_router
-from routes.gpt_routes    import router as gpt_router
 from routes.jemi_routes import router as jemi_router
 from routes.tts_routes import router as tts_router
 
 app = FastAPI()
 
 app.include_router(vision_router, prefix="/web-detection")
-app.include_router(gpt_router,    prefix="/gpt")      
-# app.include_router(jemi_router, prefix="/jemi")
+app.include_router(jemi_router, prefix="/jemi")
 app.include_router(tts_router, prefix="/tts")
