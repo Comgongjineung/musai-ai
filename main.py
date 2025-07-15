a7c524ea4e09c9d15d7cd49b051690f9f4241a0d
RYU <rjw5256@gmail.com>
2025-07-15 16:03:09 +0900

    ❇️ RECYCLE: Update 라우트 변경
    FastAPI 라우트 변경했습니다

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routes.vision_routes import router as vision_router
 from routes.gpt_routes    import router as gpt_router
 from routes.jemi_routes import router as jemi_router
 from routes.tts_routes import router as tts_router
+from routes.ar_routes     import router as ar_router
 
 app = FastAPI()
 
@@ -11,3 +12,4 @@ app.include_router(jemi_router, prefix="/jemi")
 app.include_router(gpt_router,    prefix="/gpt")      
 # app.include_router(jemi_router, prefix="/jemi")
 app.include_router(tts_router, prefix="/tts")
+app.include_router(ar_router, prefix="/ar")
